3df3fa45c3dcdb69b14f99b165ca0cdebe015ae4
Walid <walidoo_2002@hotmail.com>
2024-11-27 18:04:58 -0500

    feat(openai): Added ChatGPT client

---
+++ chatgpt/chatgpt_chat.py
@@ -0,0 +1,30 @@
+from IPython.core.display import Markdown
+from IPython.core.display_functions import display
+from openai import OpenAI
+
+
+class ChatGPT:
+    def __init__(self, key: str, model: str):
+        self.__client = OpenAI(api_key=key)
+        self.__model = model
+        self.__messages = []
+
+    def chat(self, prompt: str, seed: int | None = None):
+        self.__messages.append({"role": "user", "content": prompt})
+
+        response = self.__client.chat.completions.create(
+            model=self.__model,
+            messages=self.__messages,
+            seed=seed
+        )
+        answer = response.choices[0].message.content
+        self.__messages.append({"role": "assistant", "content": answer})
+        return answer
+
+    def chatAndDisplay(self, prompt: str, seed: int | None = None):
+        answer = self.chat(prompt, seed)
+        display(Markdown(answer))
+
+    def clearHistory(self):
+        self.__messages = []
+        display(Markdown("**History Cleared!**"))
